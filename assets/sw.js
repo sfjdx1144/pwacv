5a2f1ff8a2bf4e51bc5ea032d637c4d368352a76
sfjdx1144 <sfjdx1144@live.com>
2022-03-26 22:24:54 +0800

    first commit

---
+++ assets/sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME = "cacheName";
+var CACHE_URLS = [
+  "/",
+
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_URLS);
+    })
+  )
+});
+
+// 监听fetch事件
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      })
+    })
+  )
+});
